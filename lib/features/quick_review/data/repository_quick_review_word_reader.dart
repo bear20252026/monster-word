12c67ad2abcfd84b6dda11bb84bd8ba3ad55aa5e
bear20252026 <3.07672046e+08+bear20252026@users.noreply.github.com>
2026-08-28 03:41:03 +0000

    refactor(quick-review): isolate word source boundary

---
+++ lib/features/quick_review/data/repository_quick_review_word_reader.dart
@@ -0,0 +1,17 @@
+import '../../../models/word.dart';
+import '../../../repositories/word_repository.dart';
+import '../application/quick_review_word_reader.dart';
+
+/// 基于既有单词仓储的考试速刷词源适配器。
+class RepositoryQuickReviewWordReader implements QuickReviewWordReader {
+  RepositoryQuickReviewWordReader({required WordRepository repository}) : _repository = repository;
+
+  final WordRepository _repository;
+
+  @override
+  Future<List<Word>> loadWords({int limit = 50}) async {
+    final words = await _repository.searchWords('');
+    words.sort((a, b) => b.id.compareTo(a.id));
+    return words.take(limit).toList();
+  }
+}
