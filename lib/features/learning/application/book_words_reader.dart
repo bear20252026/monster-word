fb83e8cfca49061724d46321a240caa297feb99d
bear20252026 <3.07672046e+08+bear20252026@users.noreply.github.com>
2026-08-28 03:56:40 +0000

    refactor(learning): make book words reader an application port

--- lib/features/learning/application/book_words_reader.dart
+++ lib/features/learning/application/book_words_reader.dart
@@ -1,13 +1,6 @@
 import '../../../models/word.dart';
-import '../../../repositories/word_repository.dart';
 
-/// 词书单词列表的只读应用服务。
-class BookWordsReader {
-  const BookWordsReader({required WordRepository wordRepository}) : _wordRepository = wordRepository;
-
-  final WordRepository _wordRepository;
-
-  Future<List<Word>> loadWords(int bookId) {
-    return _wordRepository.getWordsByBookId(bookId, limit: 1000);
-  }
+/// 词书单词列表的只读应用端口。
+abstract interface class BookWordsReader {
+  Future<List<Word>> loadWords(int bookId);
 }
---
+++ lib/features/learning/data/repository_book_words_reader.dart
@@ -0,0 +1,13 @@
+import '../../../models/word.dart';
+import '../../../repositories/word_repository.dart';
+import '../application/book_words_reader.dart';
+
+/// 基于既有单词仓储的词书单词读取适配器。
+class RepositoryBookWordsReader implements BookWordsReader {
+  RepositoryBookWordsReader({required WordRepository repository}) : _repository = repository;
+
+  final WordRepository _repository;
+
+  @override
+  Future<List<Word>> loadWords(int bookId) => _repository.getWordsByBookId(bookId, limit: 1000);
+}
