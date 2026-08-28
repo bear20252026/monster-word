9a99a61a6eca361f62db01a19ff86804cdb36ebc
BookNameProducer (Monster Word) <books@monsterworld.team>
2026-08-28 12:30:54 +0800

    Revert "feat: 添加每日学习目标功能（滚轮选择器 1-200 个单词/天）"
    This reverts commit 2ea5d5efa3b63b3eefe8fbfa6f03774c1142cd23.

--- lib/features/learning/presentation/learning_session_state.dart
+++ lib/features/learning/presentation/learning_session_state.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 
-import '../../../data/app_preferences.dart';
 import '../../../engine/core_engine.dart';
 import '../../../engine/fsrs6_engine.dart';
 import '../../../engine/leitner_engine.dart';
@@ -61,12 +60,9 @@ class LearningSessionState extends ChangeNotifier {
     _replaceQueue(favorites.take(limit).toList(growable: false));
   }
 
-  Future<void> loadBook(Book book, {int? limit, bool shuffle = true}) async {
+  Future<void> loadBook(Book book, {int limit = 50, bool shuffle = true}) async {
     _currentBook = book;
-    // ✅ 使用每日学习目标作为默认限制
-    final dailyGoal = UserPreferences().getDailyGoal();
-    final actualLimit = limit ?? dailyGoal;
-    final queue = await _queueRepository.loadBook(book, limit: actualLimit, shuffle: shuffle);
+    final queue = await _queueRepository.loadBook(book, limit: limit, shuffle: shuffle);
     _replaceQueue(queue);
     unawaited(_saveProgress());
   }
